c3d6e428a90d4bea0fd9c37bd31b182691b73aad
Tomas <oyarzun.tomas@gmail.com>
2018-07-30 04:41:19 -0400

    ADD: Ejercicio7 and nuevo_catalogo.txt

---
+++ ejercicio7.rb
@@ -0,0 +1,34 @@
+class Product
+	attr_accessor :name, :large, :medium, :small, :xsmall
+  def initialize(name, large, medium, small, xsmall)
+    @name = name
+    @large = large.to_i
+    @medium = medium.to_i
+    @small = small.to_i
+    @xsmall = xsmall.to_i	
+  end
+
+  def average
+  	puts "El promedio de #{@name} es: #{(@large + @medium + @small + @xsmall) / 4}"
+  end
+
+  def new_catalog(data)
+      file = File.open('nuevo_catalogo.txt', 'a')
+      file.puts "#{data.name} #{data.large} #{data.medium} #{data.small}" 
+      file.close
+  end
+end
+
+products_list = []
+data = []
+File.open('catalogo.txt', 'r') { |file| data = file.readlines}
+data.each do |prod|
+  ls = prod.split(', ')
+  products_list << Product.new(*ls)
+end
+
+products_list.each{ |info| info.new_catalog(info) }
+
+
+
+
