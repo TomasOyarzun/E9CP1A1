c42ba91add5c89e12eba8f0b1f8ad102bd703a37
Tomas <oyarzun.tomas@gmail.com>
2018-07-30 02:59:43 -0400

    ADD: Ejercicio2, Ejercicio3, Ejercicio4

---
+++ ejercicio4.rb
@@ -0,0 +1,13 @@
+class Dog
+	def initialize(propiedades)
+		@propiedades = propiedades
+	end
+
+	def ladrar
+		puts "#{@propiedades[:nombre]} está ladrando"
+	end
+end
+
+propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
+perro = Dog.new(propiedades)
+perro.ladrar
