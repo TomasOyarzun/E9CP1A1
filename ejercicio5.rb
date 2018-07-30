dbfd1db3c6935a96039caefb28b3588818917556
Tomas <oyarzun.tomas@gmail.com>
2018-07-30 03:10:55 -0400

    ADD: Ejercicio5

---
+++ ejercicio5.rb
@@ -0,0 +1,18 @@
+class Morseable
+  def initialize(number)
+    @number = number
+  end
+
+  def generate_hash(number)
+    h = {0 => '-----', 1 => '.----', 2 => '..---', 3 => '...--', 4 => '....-', 5 => '.....', 
+      6 => '-....', 7 => '--...', 8 => '---..', 9 => '----.' }
+    puts h[number]
+  end
+
+  def to_morse
+    generate_hash(@number)
+  end
+end
+
+m = Morseable.new(3)
+m.to_morse
